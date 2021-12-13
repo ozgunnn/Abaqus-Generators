6c7e0cfb0806f1baac57fdbb66b1327c20eb2312
ozgunnn <ozgunergun@gmail.com>
2021-12-13 09:43:18 +0100

    Update odb_destroyer.py

--- odb_destroyer.py
+++ odb_destroyer.py
@@ -1,13 +1,15 @@
 from odbAccess import *
 
-odb = openOdb(path='C:/TestODBs/Job-A15.odb')
+odb = openOdb(path='C:/temp/Job_B14.odb')
 
 mycenter = odb.rootAssembly.nodeSets['REFERENCE_POINT_        1']
+
 max=0
-for i in range(len(odb.steps['Step-1'].frames)):
+#for i in range(len(odb.steps['Step-1'].frames)):
+for i in range(100):
     #u = odb.steps['Step-1'].frames[i].fieldOutputs['U'].getSubset(region=mycenter).values[0].data[2]
-    f = odb.steps['Step-1'].frames[i].fieldOutputs['RF'].getSubset(region=mycenter).values[0].data[2]
+    f = abs(odb.steps['Step-1'].frames[i].fieldOutputs['RF'].getSubset(region=mycenter).values[0].data[2])
     if f > max:
         max = f
-    
-max
+
+f
